97bfa9c9845bc9db2a4a0cac7be6dc41d1a8c4dd
Jennifer Emanuele <jemanuele@gmail.com>
2021-11-19 12:11:50 -0500

    added to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -225,6 +225,30 @@ var saveTasks = function() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 
 }
+
+var loadTasks= function() {
+  var savedTasks = localStorage.getItem("tasks");
+  //if there are no tasks, set tasks to an empty array and returen out of the function
+  if(!savedTasks) {
+    return false;
+  }
+  console.log("Saved tasks found!");
+  //else, load up saved tasks
+
+  //parse into array of objects
+  savedTasks = JSON.parse(savedTasks);
+
+  //loop through savedTasks array
+  for (var i = 0; i< savedTasks.length; i++) {
+    //pass each task object into the 'createTaskEl()' function
+    createTaskEl(savedTasks[i]);
+    console.log(tasks[i]);
+  }
+};
+
+
+
+
 // Create a new task
 formEl.addEventListener("submit", taskFormHandler);
 
@@ -233,3 +257,5 @@ pageContentEl.addEventListener("click", taskButtonHandler);
 
 // for changing the status
 pageContentEl.addEventListener("change", taskStatusChangeHandler);
+
+loadTasks();
